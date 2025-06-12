0920fca16f03d9f3cd90631f4bcd72d0f88c26ce
Jize <meshcheryakov20122003@gmail.com>
2025-06-12 12:48:15 +0300

    попытки 1

--- alembic/env.py
+++ alembic/env.py
@@ -1,19 +1,21 @@
 from logging.config import fileConfig
-from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 from app.database import Base, DATABASE_URL
+import os
 from app import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+if DATABASE_URL:
+    config.set_main_option('sqlalchemy.url', DATABASE_URL)
+else:
+    raise RuntimeError("database_url not set in app.database")
 
 # add your model's MetaData object here
 # for 'autogenerate' support
